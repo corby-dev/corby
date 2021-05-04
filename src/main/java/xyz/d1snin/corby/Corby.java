e82f675b726ba583e87d482ec35498b1fa45a2a5
Mikhail Koshkin <d1snin@aol.com>
2021-05-04 21:57:50 +0300

    unused import

--- src/main/java/xyz/d1snin/corby/Corby.java
+++ src/main/java/xyz/d1snin/corby/Corby.java
@@ -15,7 +15,6 @@ import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import xyz.d1snin.corby.commands.Command;
